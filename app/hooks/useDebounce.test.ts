6f269e507aa7010f970c99e2e71660b48243b53e
haiturtle <haiturtle@turtle.com>
2020-12-27 20:50:28 -0500

    IMPROVE: reimplement hook.

--- app/hooks/useDebounce.test.ts
+++ app/hooks/useDebounce.test.ts
@@ -1,46 +1,50 @@
-import { renderHook } from '@testing-library/react-hooks'
+import { renderHook, act } from '@testing-library/react-hooks'
 
 import { useDebounce } from './useDebounce'
 
-// tell jest to mock all timeout functions
-jest.useFakeTimers();
-
 describe('useDebounce', () => {
-  const props = {
-    initialProps: { value: ''}
-  };
+  beforeEach(() => {
+    // tell jest to mock all timeout functions
+    jest.useFakeTimers('modern');
+  })
 
   it('returns initial value', () => {
-    const { result } = renderHook(({value }) => useDebounce(value, 500), props)
+    const { result } = renderHook(() => useDebounce('', 200))
 
     expect(result.current[0]).toBe('');
   })
 
   it('does not update the value before the delay', () => {
-    const { result, rerender } = renderHook(({value}) => useDebounce(value, 500), props)
+    const { result } = renderHook(() => useDebounce('', 200))
 
-    rerender({ value: 'h'})
-    jest.advanceTimersByTime(499);
+    act(() => {
+      result.current[1]('h')
+      jest.advanceTimersByTime(199);
+    })
     
     expect(result.current[0]).toBe('');
   })
 
   it('updates the value after the delay', () => {
-    const { result, rerender } = renderHook(({value}) => useDebounce(value, 500), props)
+    const { result } = renderHook(() => useDebounce('', 200))
 
-    rerender({ value: 'hi'});
-    jest.advanceTimersByTime(500);
+    act(() => {
+      result.current[1]('hi')
+      jest.advanceTimersByTime(200);
+    })
 
     expect(result.current[0]).toBe('hi');
   })
 
   it('resets the delay on value change', () => {
-    const { result, rerender } = renderHook(({value}) => useDebounce(value, 500), props)
+    const { result } = renderHook(() => useDebounce('', 200))
 
-    jest.advanceTimersByTime(250);
-    rerender({ value: 'h'})
-    jest.advanceTimersByTime(499);
-    
+    act(() => {
+      jest.advanceTimersByTime(100);
+      result.current[1]('hi')
+      jest.advanceTimersByTime(110);
+    })
+
     expect(result.current[0]).toBe('');
   })
 })
--- app/hooks/useDebounce.ts
+++ app/hooks/useDebounce.ts
@@ -1,26 +1,20 @@
-import { useState, useEffect, Dispatch, SetStateAction } from 'react';
+import { useState, useCallback } from 'react';
+import debounce from "lodash/debounce";
 
-export function useDebounce<T>(value: T, delay: number): [T, Dispatch<SetStateAction<T>>] {
-    // State and setters for debounced value
-    const [debouncedValue, setDebouncedValue] = useState(value);
-  
-    useEffect(
-      () => {
-        // Update debounced value after delay
-        const handler = setTimeout(() => {
-          setDebouncedValue(value);
-        }, delay);
-  
-        // Cancel the timeout if value changes (also on delay change or unmount)
-        // This is how we prevent debounced value from updating if value is changed ...
-        // .. within the delay period. Timeout gets cleared and restarted.
-        return () => {
-          clearTimeout(handler);
-        };
-      },
-  
-      [value, delay] // Only re-call effect if value or delay changes
-    );
-  
-    return [debouncedValue, setDebouncedValue];
+export function useDebounce<T>(value: T, delay: number) {
+  const [state, setState] = useState(value);
+
+  const setDebouncedState = (_val: T) => {
+    _debounce(_val);
+  };
+
+  const _debounce = useCallback(
+    debounce((_prop: T) => {
+      console.log("updating input");
+      setState(_prop);
+    }, delay),
+    []
+  );
+
+  return [state, setDebouncedState] as const;
   }
