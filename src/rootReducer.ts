4cc48fed04d331dc184129be98b9d9e50cd1f0ef
Haiturtle <haiturtle@Haiturtles-Mac-mini.local>
2020-12-30 17:09:15 -0500

    sql problem.

---
+++ src/rootReducer.ts
@@ -0,0 +1,29 @@
+import { combineReducers } from "redux";
+import userReducer from "./reducers/user";
+import familiarsReducer from "./reducers/familiars";
+import inputsReducer from "./reducers/inputs";
+import messageReducer from "./reducers/messages";
+import sessionReducer from "./reducers/sessions";
+import appReducer from "./reducers/app";
+import serverReducer from "./reducers/servers";
+import channelReducer from "./reducers/channels";
+import permissionsReducer from "./reducers/permissions";
+import groupMessageReducer from "./reducers/groupMessages";
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export default function createRootReducer() {
+    const root = combineReducers({
+        user: userReducer,
+        familiars: familiarsReducer,
+        inputs: inputsReducer,
+        messages: messageReducer,
+        sessions: sessionReducer,
+        app: appReducer,
+        servers: serverReducer,
+        channels: channelReducer,
+        groupMessages: groupMessageReducer,
+        permissions: permissionsReducer,
+    });
+
+    return root;
+}
---
+++ src/views/Login.tsx
@@ -0,0 +1,98 @@
+import { faLock } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState, FunctionComponent, memo, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router";
+import { routes } from "../constants/routes";
+import { addInputState } from "../reducers/inputs";
+import { gaurdian } from "./Base";
+import { BackButton } from "../components/BackButton";
+import { keyFolder } from "../constants/folders";
+import { VerticalAligner } from "../components/VerticalAligner";
+import { useQuery } from "../hooks/useQuery";
+import { useDebounce } from "../hooks/useDebounce";
+import { RootState } from "../store";
+
+const FORM_NAME = "keyfile-login-pasword";
+
+export const Login: FunctionComponent = memo(() => {
+    const history = useHistory();
+
+    const query = useQuery();
+    const password = useSelector<RootState, string>(
+        ({ inputs }) => inputs[FORM_NAME] || ""
+    );
+    const publicKey = query.get("key");
+    const [loading, setLoading] = useState(false);
+    const [errText, setErrText] = useState("");
+    const dispatch = useDispatch();
+
+    const [inputVal, setInputVal] = useState("");
+    const debouncedInput = useDebounce(inputVal, 500);
+
+    useEffect(() => {
+        dispatch(addInputState(FORM_NAME, debouncedInput));
+    }, [debouncedInput]);
+
+    const unlockKey = (enterPw?: string) => {
+        const sentPassword = enterPw || password;
+
+        if (sentPassword == "") return;
+
+        setLoading(true);
+        setInputVal("");
+
+        try {
+            gaurdian.load(keyFolder + "/" + publicKey, sentPassword);
+        } catch (err) {
+            console.error(err);
+            setErrText(err.toString());
+            setLoading(false);
+            return;
+        }
+
+        history.push(routes.HOME);
+    };
+
+    return (
+        <VerticalAligner top={<BackButton route={routes.HOME} />}>
+            <div className="box">
+                {errText !== "" && (
+                    <div className="notification is-danger">{errText}</div>
+                )}
+                <label className="label is-small">Password:</label>
+                <div className="control input-wrapper has-icons-left has-icons-right">
+                    <input
+                        className="input"
+                        type="password"
+                        placeholder="hunter2"
+                        value={inputVal}
+                        onChange={({ target: { value } }) => {
+                            setInputVal(value);
+                        }}
+                        onKeyDown={(event) => {
+                            if (event.key === "Enter") {
+                                unlockKey(inputVal);
+                            }
+                        }}
+                    />
+                    <span className="icon is-left">
+                        <FontAwesomeIcon icon={faLock} />
+                    </span>
+                </div>
+                <div className="buttons is-right">
+                    <button
+                        className={`button is-success ${
+                            loading ? "is-loading" : ""
+                        }`}
+                        onClick={() => {
+                            unlockKey();
+                        }}
+                    >
+                        Unlock
+                    </button>
+                </div>
+            </div>
+        </VerticalAligner>
+    );
+});
