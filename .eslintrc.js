1e60f042f5f783b013187db2dc7a805e5a5c333e
haiturtle <haiturtle@haiturtle.com>
2020-12-31 18:19:52 -0500

    IMPROVE: configure prettier to run on lint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'plugin:eslint-comments/recommended',
+        "plugin:prettier/recommended",
+        "prettier/@typescript-eslint",
     ],
     plugins: ['import'],
     parserOptions: {
@@ -37,7 +39,7 @@ module.exports = {
         // REACT OVERRIDES
         {
             files: ['*.tsx'],
-            extends: ['plugin:react/recommended'],
+            extends: ['plugin:react/recommended', "prettier/react"],
             rules: {
                 'react/prop-types': 0,
                 'react/display-name': 0,
--- assets/assets.d.ts
+++ assets/assets.d.ts
@@ -1,14 +1,14 @@
-declare module '*.svg' {
+declare module "*.svg" {
     const content: never;
     export default content;
 }
 
-declare module '*.png' {
+declare module "*.png" {
     const content: never;
     export default content;
 }
 
-declare module '*.jpg' {
+declare module "*.jpg" {
     const content: never;
     export default content;
 }
