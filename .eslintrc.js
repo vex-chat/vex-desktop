152eae3c79d890fd2209215bf67f1a4e5d426c7d
Haiturtle <haiturtle@Haiturtles-Mac-mini.local>
2020-12-31 04:34:51 -0500

    REMOVE: useless deps, configurations + fix any linting err/warns.
     You are currently rebasing branch 'fix/dev-config-post-electron-update' on 'c023121'.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,35 @@
 module.exports = {
+    // GLOBAL CONFIG
     env: {
         browser: true,
         es2021: true,
     },
+    parser: '@typescript-eslint/parser',
     extends: [
         'eslint:recommended',
-        'plugin:react/recommended',
         'plugin:@typescript-eslint/recommended',
     ],
-    parser: '@typescript-eslint/parser',
     parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
         ecmaVersion: 12,
         sourceType: 'module',
     },
-    plugins: ['react', '@typescript-eslint'],
-    rules: {
-        'react/prop-types': 0,
-        'react/display-name': 0,
+    settings: {
+        react: {
+          version: "detect"
+        }
     },
+    plugins: [],
+    overrides: [
+        // REACT OVERRIDES
+        {
+            files: ["*.tsx"],
+            extends: [
+                'plugin:react/recommended',
+            ],
+            rules: {
+                "react/prop-types": 0,
+                "react/display-name": 0,
+            },
+        }
+    ],
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { HashRouter as Router } from 'react-router-dom';
+
 import { configuredStore } from './store';
 import './app.global.scss';
-import Root from './Root';
+import Base from './views/Base';
 
 const store = configuredStore();
 
 document.addEventListener('DOMContentLoaded', async () => {
-    render(<Root store={store} />, document.getElementById('root'));
+    render(
+        <Provider store={store}>
+            <Router>
+                <Base />
+            </Router>
+        </Provider>,
+        document.getElementById('root')
+    );
 });
