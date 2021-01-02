7fc6677110b9bfe37b080f98b3d498be733a2e50
haiturtle <haiturtle@haiturtle.com>
2021-01-02 08:27:17 -0500

    IMPROVE: combine modules and reduce loc in dev configs.

--- .erb/dev/webpack.renderer.dll.js
+++ .erb/dev/webpack.renderer.dll.js
@@ -7,21 +7,17 @@ const { DllPlugin, LoaderOptionsPlugin } = require("webpack");
 const { merge } = require("webpack-merge");
 
 const CheckNodeEnv = require("../scripts/CheckNodeEnv");
-const rootPackage = require("../../package.json");
-
-const base = require("./webpack.base");
-
 CheckNodeEnv("development");
 
 const dist = join(__dirname, "../dll");
 
-module.exports = merge(base, {
+/** @type {import('webpack').Configuration} */
+const dll = {
     context: join(__dirname, "../.."),
     devtool: "eval",
     externals: ["fsevents", "crypto-browserify"],
-    module: require("./webpack.renderer.js").module,
     entry: {
-        renderer: Object.keys(rootPackage.dependencies || {}),
+        renderer: Object.keys(require("../../package.json").dependencies || {}),
     },
     output: {
         path: dist,
@@ -29,7 +25,6 @@ module.exports = merge(base, {
         library: "renderer",
         filename: "[name].dev.dll.js",
     },
-
     plugins: [
         new DllPlugin({
             path: join(dist, "[name].json"),
@@ -45,4 +40,6 @@ module.exports = merge(base, {
             },
         }),
     ],
-});
+};
+
+module.exports = merge(require("./webpack.base"), dll);
--- .erb/dev/webpack.renderer.js
+++ .erb/dev/webpack.renderer.js
@@ -1,9 +1,7 @@
-const { spawn, execSync } = require("child_process");
-const { existsSync } = require("fs");
+const { spawn } = require("child_process");
 const { join, resolve } = require("path");
 
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
-const chalk = require("chalk");
 const {
     LoaderOptionsPlugin,
     DllReferencePlugin,
@@ -11,200 +9,31 @@ const {
 } = require("webpack");
 const { merge } = require("webpack-merge");
 
-const base = require("./webpack.base");
-
 const port = process.env.PORT || 1212;
 const publicPath = `http://localhost:${port}/dist`;
 const dllDir = join(__dirname, "../dll");
-const manifest = resolve(dllDir, "renderer.json");
-const requiredByDLLConfig = module.parent.filename.includes(
-    "webpack.renderer.dll"
-);
 
-/**
- * Warn if the DLL is not built
- */
-if (!requiredByDLLConfig && !(existsSync(dllDir) && existsSync(manifest))) {
-    console.log(
-        chalk.black.bgYellow.bold(
-            'The DLL files are missing. Sit back while we build them for you with "yarn build-dll"'
-        )
-    );
-    execSync("yarn build-dll");
-}
-
-module.exports = merge(base, {
+/** @type {import('webpack').Configuration} */
+const renderer = {
     devtool: "eval-source-map",
     entry: [require.resolve("../../src/index.tsx")],
     output: {
         path: join(__dirname, "../../src"),
         libraryTarget: "commonjs2",
-        publicPath: `http://localhost:${port}/dist/`,
+        publicPath,
         filename: "renderer.dev.js",
     },
-    module: {
-        rules: [
-            {
-                test: /\.[jt]sx?$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: require.resolve("babel-loader"),
-                        options: {
-                            plugins: [
-                                require.resolve("react-refresh/babel"),
-                            ].filter(Boolean),
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.global\.css$/,
-                use: [
-                    {
-                        loader: "style-loader",
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            sourceMap: true,
-                        },
-                    },
-                ],
-            },
-            {
-                test: /^((?!\.global).)*\.css$/,
-                use: [
-                    {
-                        loader: "style-loader",
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: {
-                                localIdentName:
-                                    "[name]__[local]__[hash:base64:5]",
-                            },
-                            sourceMap: true,
-                            importLoaders: 1,
-                        },
-                    },
-                ],
-            },
-            // SASS support - compile all .global.scss files and pipe it to style.css
-            {
-                test: /\.global\.(scss|sass)$/,
-                use: [
-                    {
-                        loader: "style-loader",
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            sourceMap: true,
-                        },
-                    },
-                    {
-                        loader: "sass-loader",
-                    },
-                ],
-            },
-            // SASS support - compile all other .scss files and pipe it to style.css
-            {
-                test: /^((?!\.global).)*\.(scss|sass)$/,
-                use: [
-                    {
-                        loader: "style-loader",
-                    },
-                    {
-                        loader: "@teamsupercell/typings-for-css-modules-loader",
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: {
-                                localIdentName:
-                                    "[name]__[local]__[hash:base64:5]",
-                            },
-                            sourceMap: true,
-                            importLoaders: 1,
-                        },
-                    },
-                    {
-                        loader: "sass-loader",
-                    },
-                ],
-            },
-            // WOFF Font
-            {
-                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-                use: {
-                    loader: "url-loader",
-                    options: {
-                        limit: 10000,
-                        mimetype: "application/font-woff",
-                    },
-                },
-            },
-            // WOFF2 Font
-            {
-                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-                use: {
-                    loader: "url-loader",
-                    options: {
-                        limit: 10000,
-                        mimetype: "application/font-woff",
-                    },
-                },
-            },
-            // TTF Font
-            {
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                use: {
-                    loader: "url-loader",
-                    options: {
-                        limit: 10000,
-                        mimetype: "application/octet-stream",
-                    },
-                },
-            },
-            // EOT Font
-            {
-                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                use: "file-loader",
-            },
-            // SVG Font
-            {
-                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                use: {
-                    loader: "url-loader",
-                    options: {
-                        limit: 10000,
-                        mimetype: "image/svg+xml",
-                    },
-                },
-            },
-            // Common Image Formats
-            {
-                test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/,
-                use: "url-loader",
-            },
-        ],
-    },
     plugins: [
-        requiredByDLLConfig
-            ? null
-            : new DllReferencePlugin({
-                  context: dllDir,
-                  manifest: require(manifest),
-                  sourceType: "var",
-              }),
+        new DllReferencePlugin({
+            context: dllDir,
+            manifest: require(resolve(dllDir, "renderer.json")),
+            sourceType: "var",
+        }),
 
         new NoEmitOnErrorsPlugin(),
         new LoaderOptionsPlugin({
             debug: true,
         }),
-
         new ReactRefreshWebpackPlugin(),
     ],
     node: {
@@ -242,4 +71,6 @@ module.exports = merge(base, {
                 .on("error", (spawnError) => console.error(spawnError));
         },
     },
-});
+};
+
+module.exports = merge(require("./webpack.base"), renderer);
