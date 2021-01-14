dff48cb04ee766005126a849c6db88e0a153f44c
ExtraHash <ExtraHash@protonmail.com>
2021-01-14 03:14:56 -0700

    switch to .wav instead of ogg because of weird git bug

--- src/constants/sounds.ts
+++ src/constants/sounds.ts
@@ -3,7 +3,7 @@ import { getAssetPath } from "../components/MessageBox";
 export const unlockFX = new Audio(getAssetPath("sounds/switch_006.ogg"));
 unlockFX.load();
 
-export const lockFX = new Audio(getAssetPath("sounds/switch_007.ogg"));
+export const lockFX = new Audio(getAssetPath("sounds/switch_007.wav"));
 lockFX.load();
 
 export const notifyFX = new Audio(getAssetPath("sounds/confirmation_001.ogg"));
