dc77c1205a8e9611d054edb375eecf93cdb85e9b
ExtraHash <ExtraHash@protonmail.com>
2021-01-21 13:53:26 -0700

    release is now 11 assets

--- src/Base.tsx
+++ src/Base.tsx
@@ -134,7 +134,7 @@ export default function Base(): JSX.Element {
         );
         if (
             semver.gt(res.data.tag_name, currentVersion) &&
-            res.data.assets.length === 9
+            res.data.assets.length === 11
         ) {
             setUpdateAvailable(true);
         }
